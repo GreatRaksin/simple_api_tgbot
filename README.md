3412886a0101d4e5b08035ec04c6f2e4aa251616
greatraksin <greatraksin@icloud.com>
2023-03-18 12:37:11 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Telegram bot, using PyTeleBotApi, requests, geopy
+
---
+++ main.py
@@ -0,0 +1,6 @@
+
+from bot_functions import *
+
+
+if __name__ == '__main__':  # если запускается ИМЕННО ЭТОТ файл
+    bot.polling()  # опрашивать сервер
